7d3d653e61db392ae810326cb138b7279367159a
Luis Villas <shusten@hotmail.com>
2019-12-13 16:32:31 -0300

    Implementando e testando métodos sobrecarregados na main

--- src/Principal.java
+++ src/Principal.java
@@ -1,9 +1,14 @@
 public class Principal {
     public static void main(String[] args) {
-       Lobo x = new Lobo();
         Cachorro c = new Cachorro();
-        System.out.println(c.emitirSom());
-        System.out.println(x.emitirSom());
 
+        c.reagir("Olá");
+        c.reagir("Vai apanhar");
+        c.reagir(11,45);
+        c.reagir(21,00);
+        c.reagir(true);
+        c.reagir(false);
+        c.reagir(2,12.5f);
+        c.reagir(17,4.5f);
     }
 }
